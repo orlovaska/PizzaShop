6c058c0cf4d56bd01a17942a033e77bf03e8b96b
Orlov Yasha <orlovaska599@gmail.com>
2023-01-12 21:04:55 +0500

    Сделал userControl Navigation

--- ShopWPFUI/ViewModels/MainViewModel.cs
+++ ShopWPFUI/ViewModels/MainViewModel.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using System.Windows.Input;
+
 
 namespace ShopWPFUI.ViewModels
 {
-    internal class MainViewModel
+    internal class MainViewModel: BaseViewModel
     {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand AutorizationCommand { get; set; }
+        public ICommand RegistrationCommand { get; set; }
+        public ICommand NovigationCommand { get; set; }
+
+
+        private void Autoruzation(object obj) => CurrentView = new AuthorizationViewModel();
+        private void Registration(object obj) => CurrentView = new RegistrationViewModel();
+        private void Navigation(object obj) => CurrentView = new NavigationViewModel();
+
+        public MainViewModel()
+        {
+            RegistrationCommand = new RelayCommand(Registration);
+            NovigationCommand = new RelayCommand(Navigation);
+
+            // Startup Page
+            CurrentView = new RelayCommand(Autoruzation);
+        }
     }
 }
