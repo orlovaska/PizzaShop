7b1959a43afe40ea94f53cf43a98ab6008a6ed27
Orlov Yasha <orlovaska599@gmail.com>
2023-01-14 17:56:08 +0500

    Сделал кнопки мин, макс и закрытие окна

--- ShopWPFUI/MainWindow.xaml.cs
+++ ShopWPFUI/MainWindow.xaml.cs
@@ -24,5 +24,21 @@ namespace ShopWPFUI
         {
             InitializeComponent();
         }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        
+        
+
     }
 }
