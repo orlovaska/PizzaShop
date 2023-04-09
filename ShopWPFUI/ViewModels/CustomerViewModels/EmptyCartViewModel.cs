8c08761811e448d646e675d4a2e32bf56ef1b302
Orlov Yakov <orlovaska599@gmail.com>
2023-04-09 12:17:33 +0500

    Пытаюсь что-то сделать

---
+++ ShopWPFUI/ViewModels/CustomerViewModels/EmptyCartViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ShopWPFUI.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using static ShopWPFUI.ViewModels.CustomerViewModels.CartViewModel;
+
+namespace ShopWPFUI.ViewModels.CustomerViewModels
+{
+    internal class EmptyCartViewModel: BaseViewModel
+    {
+        public event EventHandler ShowsCatalog;
+
+        public ICommand ShowsCatalogCommand { get; }
+
+        public EmptyCartViewModel()
+        {
+            ShowsCatalogCommand = new RelayCommand(OnShowsCatalog);
+        }
+
+        private void OnShowsCatalog(object obj)
+        {
+            ShowsCatalog?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ ShopWPFUI/ViewModels/CustomerViewModels/ProfileViewModel.cs
@@ -0,0 +1,38 @@
+﻿using PizzaShop.DataAccess;
+using PizzaShop.Models;
+using ShopLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShopWPFUI.ViewModels.CustomerViewModels
+{
+    public class ProfileViewModel : BaseViewModel
+    {
+        private CustomerModel _currentCustomerAccount;
+
+        public CustomerModel CurrentCustomerAccount
+        {
+            get
+            {
+                return _currentCustomerAccount;
+            }
+
+            set
+            {
+                _currentCustomerAccount = value;
+                OnPropertyChanged(nameof(CurrentCustomerAccount));
+            }
+        }
+
+        private IDataConnection dataRepository { get; set; }
+
+        public ProfileViewModel(CustomerModel currentCustomerAccount)
+        {
+            CurrentCustomerAccount = currentCustomerAccount;
+        }
+    }
+}
